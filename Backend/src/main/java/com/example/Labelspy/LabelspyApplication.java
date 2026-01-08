edd45dbcdc65126c257276771a32a17b9c32ba5e
Sharan <sharansingh9.8.6.0@gmail.com>
2026-01-08 16:19:55 +0530

    feat: add LabelspyApplication.java

---
+++ Backend/src/main/java/com/example/Labelspy/LabelspyApplication.java
@@ -0,0 +1,13 @@
+package com.example.Labelspy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabelspyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabelspyApplication.class, args);
+	}
+
+}
